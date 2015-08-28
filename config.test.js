61b82a5d3940e37333594001bb49f5ddce1d9207
AJ ONeal <awesome@coolaj86.com>
2015-08-27 21:45:57 -0600

    test file name authn -> authcodes

--- config.test.js
+++ config.test.js
@@ -3,5 +3,5 @@
 module.exports = {
   // crypto.randomBytes(16).toString('hex');
   key: '1892d335081d8d346e556c9c3c8ff2c3'
-, filename: '/tmp/authn.test.sqlcipher'
+, filename: '/tmp/authcodes.test.sqlcipher'
 };
